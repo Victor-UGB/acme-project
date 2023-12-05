6ec9f820829224d04e1d2a3c1c17c77d2c385b43
Victor Itodo <victoritodo3@gmail.com>
2023-12-05 16:08:31 +0100

    new commit

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,24 @@
-export default function Page() {
-    return <p>Customers Page</p>
+import Table from "@/app/ui/customers/table"
+import { fetchCustomers, fetchFilteredCustomers } from "@/app/lib/data"
+
+export default async function Page({searchParams} : {
+    searchParams? : {
+        query? :string
+    }
+}) {
+    const query = searchParams?.query || ""
+    const customers = await fetchCustomers()
+    const filteredCustomers = await fetchFilteredCustomers(query)
+    // console.log(customers)
+    console.log(filteredCustomers)
+    console.log(query)
+    return (
+        <main>
+            <div>
+                <div>
+                    <Table customers={filteredCustomers}/>
+                </div>
+            </div>
+        </main>
+    )
 }
